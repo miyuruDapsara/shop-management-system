38b76cf0effc1feb57eb6a896c146d61c4f01894
miyuruDapsara <136076012+miyuruDapsara@users.noreply.github.com>
2025-06-20 15:35:11 +0530

    3

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane container;
+    public TextField txtEmail;
+    public TextField txtPassword;
+
+    public void accessDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+       setUi("SignupForm");
+    }
+
+    private  void setUi(String path) throws IOException {
+        Stage stage =(Stage) container.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/"+path+".fxml")))
+        );
+    }
 }
